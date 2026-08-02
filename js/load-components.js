a75dfb0988d141e7ac710c8d677f62974dbb769f
Natan <66452757+floriatanscosta@users.noreply.github.com>
2026-08-02 13:30:31 +0200

    Atualização geral estrutura
    - Mudança de estilo de estrutura
    - inclusão de dados e arquivos
    - Padronização css
    - Inclusão header/footer

---
+++ js/load-components.js
@@ -0,0 +1,31 @@
+// js/load-components.js
+document.addEventListener("DOMContentLoaded", function () {
+    // header
+    fetch('/components/header.html')
+        .then(response => {
+            if (!response.ok) throw new Error("Erro ao carregar o header");
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('header-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Problema com a requisição do header:', error));
+
+    // footer
+    fetch('/components/footer.html')
+        .then(response => {
+            if (!response.ok) throw new Error("Erro ao carregar o footer");
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+
+            // 3. Atualizar o ano do copyright APÓS o footer ser injetado na tela
+            const yearSpan = document.getElementById("year");
+            if (yearSpan) {
+                yearSpan.textContent = new Date().getFullYear();
+            }
+        })
+        .catch(error => console.error('Problema com a requisição do footer:', error));
+
+});
