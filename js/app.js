67a30ca3d84ce350d56db7ee3982f7c57937b56c
Natan <66452757+floriatanscosta@users.noreply.github.com>
2026-09-04 00:27:07 +0200

    Update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// js/menu.js
+// app.js
 
 // Função para abrir e fechar o menu
 function toggleMenu() {
@@ -19,13 +19,13 @@ function toggleTheme() {
         document.documentElement.removeAttribute('data-theme');
         localStorage.setItem('theme', 'light');
         if (themeBtn) {
-            themeBtn.innerHTML = `<img src="/assets/icons/escuro.svg" alt="Ativar Modo Escuro" style="width: 20px; height: 20px;">`;
+            themeBtn.innerHTML = `<img src="https://floriatan.com.br/assets/icons/escuro.svg" alt="Ativar o modo escuro" style="width: 20px; height: 20px;">`;
         }
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
         if (themeBtn) {
-            themeBtn.innerHTML = `<img src="/assets/icons/claro.svg" alt="Ativar Modo Claro" style="width: 20px; height: 20px;">`;
+            themeBtn.innerHTML = `<img src="https://floriatan.com.br/assets/icons/claro.svg" alt="Ativar o modo claro" style="width: 20px; height: 20px;">`;
         }
     }
 }
@@ -34,15 +34,29 @@ function toggleTheme() {
 function switchLanguage(lang) {
     localStorage.setItem('preferred_lang', lang);
     const currentPath = window.location.pathname;
-    
+
     if (lang === 'en' && !currentPath.startsWith('/en/')) {
-        // Se estiver na raiz, vai para /en/. Se não, adiciona /en antes do caminho (ex: /en/biografia)
         const newPath = currentPath === '/' ? '/en/' : '/en' + currentPath;
         window.location.href = newPath;
     }
     else if (lang === 'pt' && currentPath.startsWith('/en/')) {
-        // Remove exatamente o '/en' do início do caminho
         const newPath = currentPath.replace(/^\/en/, '') || '/';
         window.location.href = newPath;
     }
 }
+
+// ==== Inicialização ao carregar a página ====
+document.addEventListener('DOMContentLoaded', () => {
+    const currentTheme = localStorage.getItem('theme');
+    const themeBtn = document.getElementById('theme-toggle');
+
+    if (currentTheme === 'dark' && themeBtn) {
+        themeBtn.innerHTML = `<img src="https://floriatan.com.br/assets/icons/claro.svg" alt="Ativar o modo claro" style="width: 20px; height: 20px;">`;
+    }
+
+    // 2. Insere o ano atual no rodapé
+    const yearElement = document.getElementById("year");
+    if (yearElement) {
+        yearElement.textContent = new Date().getFullYear();
+    }
+});
